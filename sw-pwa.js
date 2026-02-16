717c8cd4d1988b5be4e9bca09dd9aeb36eb339dc
gsjaekklesia <gsjaekklesia08@gmail.com>
2026-02-16 17:06:45 +0800

    Update sw-pwa.js

--- sw-pwa.js
+++ sw-pwa.js
@@ -1,5 +1,5 @@
 // PWA Service Worker for GSJA Ekklesia Room Booking
-const CACHE_NAME = 'gsja-booking-v1.0.0';
+const CACHE_NAME = 'gsja-booking-v1.1.0';
 const urlsToCache = [
   './',
   './index.html',
